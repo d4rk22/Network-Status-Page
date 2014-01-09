7df4c90c8650363d10e1bd729a947ffa672bdea8
Moreno Sint Hill <info@mirabis.nl>
2014-01-09 22:28:39 +0100

    Fix ROOT_DIR already defined error

--- init.php
+++ init.php
@@ -1,4 +1,8 @@
 <?php
-define('ROOT_DIR', dirname(__FILE__));
-define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
+if(!defined('ROOT_DIR')) {
+    define('ROOT_DIR', dirname(__FILE__));
+}
+if(!defined('ROOT_URL')) {
+    define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
+}
 ?>
